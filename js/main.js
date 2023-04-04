8d237bbd0154305483a1b5ab78b675b5de8d8a9a
brachokarol <brachokarol.sud@gmail.com>
2023-04-05 01:09:28 +0200

    css my favorites

--- js/main.js
+++ js/main.js
@@ -183,7 +183,9 @@ const deleteBook = () => {
 
 }
 const showFavorites = () => {
-  prepareList(view.myFavorites);
+  if (view.myFavorites.length !== 0) {
+    prepareList(view.myFavorites);
+  }
 }
 
 view.searchBtn.addEventListener("click", search);
